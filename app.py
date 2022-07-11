a47b7bc19d16b12f5aa453eed0c8ad0a04afadd3
Andrew  Pacheco <ndrewpacheco@gmail.com>
2022-07-11 16:05:30 -0400

    added .gitignore

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+
+# the toolbar is only enabled in debug mode:
+app.debug = True
+
+# set a 'SECRET_KEY' to enable the Flask session cookies
+app.config['SECRET_KEY'] = '<replace with a secret key>'
+
+toolbar = DebugToolbarExtension(app)
